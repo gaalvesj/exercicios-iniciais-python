999ef685d5f9de9b22dfe1310568d63a5bdfb247
Gabriel <gabrielalves3654@gmail.com>
2022-09-20 23:17:40 -0300

    exercicio 02 feito

--- RM96512_EX02.py
+++ RM96512_EX02.py
@@ -1,54 +1,18 @@
-VOTOS_SEG = 0
-VOTOS_TER = 0
-VOTOS_QUA = 0
-VOTOS_QUI = 0
-VOTOS_SEX = 0
+VOTOS_SEG = int(input("Quantos Votos tiveram na segunda-feira?"))
+VOTOS_TER = int(input("Quantos Votos tiveram na terça-feira?"))
+VOTOS_QUA = int(input("Quantos Votos tiveram na quarta-feira?"))
+VOTOS_QUI = int(input("Quantos Votos tiveram na quinta-feira?"))
+VOTOS_SEX = int(input("Quantos Votos tiveram na sexta-feira?"))
 
-def votoSeg():
-    global VOTOS_SEG
-    VOTOS_SEG += 1
-    return VOTOS_SEG
+if VOTOS_SEG > VOTOS_TER and VOTOS_SEG > VOTOS_QUA and VOTOS_SEG > VOTOS_QUI and VOTOS_SEG > VOTOS_SEX:
+    print(f"segunda foi o mais votado com {VOTOS_SEG} votos")
+elif VOTOS_TER  > VOTOS_SEG and VOTOS_TER > VOTOS_QUA and VOTOS_TER > VOTOS_QUI and VOTOS_TER > VOTOS_SEX :
+    print(f"terca foi o mais votado com {VOTOS_TER} votos")
+elif VOTOS_QUA  > VOTOS_SEG and VOTOS_QUA > VOTOS_TER and VOTOS_QUA > VOTOS_QUI and VOTOS_QUA > VOTOS_SEX :
+    print(f"quarta foi o mais votado com {VOTOS_QUA} votos")
+elif VOTOS_QUI  > VOTOS_SEG and VOTOS_QUI > VOTOS_TER and VOTOS_QUI > VOTOS_QUA and VOTOS_QUI > VOTOS_SEX :
+    print(f"quinta foi o mais votado com {VOTOS_QUI} votos")
+elif VOTOS_SEX  > VOTOS_SEG and VOTOS_SEX > VOTOS_TER and VOTOS_SEX > VOTOS_QUA and VOTOS_SEX > VOTOS_QUI :
+    print(f"sexta foi o mais votado com {VOTOS_SEX} votos")
 
-def votoTer():
-    global VOTOS_TER
-    VOTOS_TER += 1
-    return VOTOS_TER
-
-def votoQua():
-    global VOTOS_QUA
-    VOTOS_QUA += 1
-    return VOTOS_QUA
-
-def votoQui():
-    global VOTOS_QUI
-    VOTOS_QUI += 1
-    return VOTOS_QUI
-
-def votoSex():
-    global VOTOS_SEX
-    result = VOTOS_SEX + 1
-    return result
-
-
-resposta = int(input("Qual dia voce escolhe para lives:\
- 1-Segunda-Feira \
- 2-Terca-Feira \
- 3-Quarta-Feira \
- 4-quinta-feira \
- 5-Sexta-Feira"))
-if resposta == 1:
-        print("voce votou na segunda-feira")
-elif resposta == 2:
-        print("voce votou na terca-feira")
-elif resposta == 3:
-        print("voce votou na quarta-feira")
-
-elif resposta == 4:
-        print("voce votou na quinta-feira")
-
-elif resposta == 5:
-        print("voce votou na sexta-feira")
-
-else:
-    print("digite apenas de 1 a 5 para votar")
 
